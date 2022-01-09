c25457ddf626a17314aba36ff4287827ea351d94
Ferb7o2 <71108160+JoseJaramillo04@users.noreply.github.com>
2022-01-08 19:27:06 -0600

    Fixed minor bugs, added a datalist dropdown for the input data row, completed the agregar's button trigger. It now adds data to the array and displays error messages if any

--- react-main-app/src/ItemInfo.js
+++ react-main-app/src/ItemInfo.js
@@ -3,7 +3,10 @@ import { useParams } from 'react-router-dom';    //Helps us redirect to other pa
 
 import { useStateContext } from './contexts/dataContext';
 import { useHistory} from 'react-router-dom';
-import $ from 'jquery';
+import $, { error } from 'jquery';
+
+//get Date
+import * as moment from 'moment';
 
 function ItemInfo(props){
 
@@ -15,18 +18,40 @@ function ItemInfo(props){
     //console.log(TransactionData);
 
     let {pId} = useParams();
-   //let fakePid=pId;
+
+   //Import data from Context API
     const[ProductData, setProductData]= useStateContext()[0];
     const [transaction_data, settData]= useStateContext()[1];
     const[VendorData, setVendorData]= useStateContext()[2];
 
+
+    let realIndex=0;
+    realIndex=ProductData.find((val,i) => pId==val.pId);    //go through JSON and list by pId and not by [index]
+    console.log('yes' +pId);
+    console.log('realIndex -> '+realIndex.pId);
+
+    
+
     function displayInputFields(){
+        $('#input-row-vId').val('');
+        inputCheck();
+        //Delete all data on fields just incase it is already filled
+        $('#input-row-saleInvoiceId').val('');
+        $('#input-row-saleWeight').val('');
+        $('#input-row-salePrice').val('');
+        $('#input-row-vId').val('');
+        $('#input-row-purchaseInvoiceId').val('');
+        $('#input-row-purchaseWeight').val('');
+        $('#input-row-purchasePrice').val('');
+
+        
         $('#btnUpdate').removeAttr('hidden');
         $('#input-new-data-row').removeAttr('hidden');
     }
 
     function inputCheck()
     {
+        console.log($('#input-row-vId').val().length+' - '+$('#input-row-saleInvoiceId').val().length);
         //if vendor information is filled, then it will be a purchase transaction
             //hence, disable all Sale input fields to prevent user confusion
         if($('#input-row-vId').val().length>1)  //'>1' is to prevent completely empty input but no more change case
@@ -40,10 +65,12 @@ function ItemInfo(props){
             $('#input-row-saleWeight').attr('disabled',true);
             $('#input-row-salePrice').attr('disabled',true);
 
+            return;
+
 
     
         }
-        else if($('#input-row-saleInvoiceId').val().length>1)  //if purchase Informationis filled,
+        if($('#input-row-saleInvoiceId').val().length>1)  //if purchase Informationis filled,
                                                                 //do same steps but with PURCHASE input fields
         {
             //delete any previous information on PURCHASE fields, if any
@@ -57,6 +84,8 @@ function ItemInfo(props){
             $('#input-row-purchaseInvoiceId').attr('disabled',true);
             $('#input-row-purchaseWeight').attr('disabled',true);
             $('#input-row-purchasePrice').attr('disabled',true);
+
+            return;
         }
         else    //if both fields are empty (may have erased and tries to fill opposite field)
         {
@@ -68,20 +97,91 @@ function ItemInfo(props){
             $('#input-row-purchaseInvoiceId').attr('disabled',false);
             $('#input-row-purchaseWeight').attr('disabled',false);
             $('#input-row-purchasePrice').attr('disabled',false);
-
         }
+        
     }
 
     function addNewDataRow(){
 
+        //Make a pointer of that Error Template HTML tag since we will be using it alot
+        var errorTemplate=$('#error-template');
+        errorTemplate.attr('hidden',true);  //keep it hidden
+
     
         //-------->Check for valid [non-empty] PURCHASE info data
         //------------->Display proper error messages if failed check
-        
-        
-        //-------->Check for valid [non-empty] SALE info data
-        //------------->Display proper error messages if failed check
+        if($('#input-row-vId').val()!=0)
+        {
+            let vendorId=$("[list='vendors']").val();   //get the vendor from the [datalist] dropdown
+            vendorId=idForName(vendorId);   //Convert to proper db data
 
+            //If non-empty
+            if($('#input-row-purchaseInvoiceId').val()!=0)
+            {
+                if($('#input-row-purchaseWeight').val()!=0)
+                {
+                    if($('#input-row-purchasePrice').val()!=0)
+                    {
+                        //then all data is valid, we can add into array
+                        settData([...transaction_data,{ tId:9, tpId:pId, date:$('#input-row-date').val(), vId:vendorId, purchaseInvoiceId:$('#input-row-purchaseInvoiceId').val(),
+                        purchaseWeight:$('#input-row-purchaseWeight').val(), purchasePrice:$('#input-row-purchasePrice').val(), saleInvoiceId: null, saleWeight:null, salePrice:null},]);
+        
+
+                        $('#input-new-data-row').attr('hidden',true);
+                        $('#btnUpdate').attr('hidden',true);
+                    }
+                    else{   //else, it is empty; Display correct error message to inform user
+                        errorTemplate.text("Error - El precio de el producto no puede estar vacío");
+                        errorTemplate.attr('hidden',false);
+                    }
+                }
+                else
+                {
+                    errorTemplate.text("Error - El peso de el producto no puede estar vacío");
+                    errorTemplate.attr('hidden',false);
+                }
+            }
+            else
+            {
+                errorTemplate.text("Error - El numero de factura no puede estar vacío");
+                errorTemplate.attr('hidden',false);
+            }
+
+        }
+        else
+        {
+            //-------->Check for valid [non-empty] SALE info data
+            //------------->Display proper error messages if failed check
+
+            if($('#input-row-saleInvoiceId').val()!=0)
+            {
+                if($('#input-row-saleWeight').val()!=0)
+                {
+                    if($('#input-row-salePrice').val()!=0)
+                    {
+                        settData([...transaction_data,{ tId:9, tpId:pId, date:$('#input-row-date').val(), vId:null, purchaseInvoiceId:null,
+                        purchaseWeight:null, purchasePrice:null, saleInvoiceId: $('#input-row-saleInvoiceId').val(),
+                        saleWeight:$('#input-row-saleWeight').val(), salePrice:$('#input-row-salePrice').val()},]);
+
+                        $('#input-new-data-row').attr('hidden',true);
+                        $('#btnUpdate').attr('hidden',true);
+                    }
+                    else
+                    {
+                        errorTemplate.text("Error - El precio de venta de el producto no puede estar vacío");
+                        errorTemplate.attr('hidden',false);
+                    }
+                }
+                else{
+                    errorTemplate.text("Error - El peso de venta de el producto no puede estar vacío");
+                    errorTemplate.attr('hidden',false);
+                }
+            }
+            else{
+                errorTemplate.text("Error - El # de Invoice de el producto no puede estar vacío");
+                errorTemplate.attr('hidden',false);
+            }
+        }
         //settData([...transaction_data,{ tId:9, tpId:1, date:'12/31/2021', vId:null, purchaseInvoiceId:null,
          //purchaseWeight:null, purchasePrice:null, saleInvoiceId: null, saleWeight:null, salePrice:null},]);
         
@@ -93,16 +193,16 @@ function ItemInfo(props){
             return ( VendorData.find(({vId},i) => vIdPassed===vId).vName);
     }
 
-    //let product_data=props.location.state.pData[(pId-1)];
-    //let transaction_data=props.location.state.tData;
+    function idForName(vNamePassed)
+    {
+        if( vNamePassed!=null)
+            return ( VendorData.find(({vName},i) => vNamePassed===vName).vId);
+    }
 
     function changeInvoiceId(e,lastValue,toChangeId)
-    {
-        
+    {   
         if(e.keyCode==13 || e.keyCode==9)   //if enter or tab is pressed
         {
-            console.log("entered");
-            //console.log(e.target.value);
             settData(
                 transaction_data.map((val,i) => val.tId==toChangeId ? { tId:val.tId, tpId:val.tpId, date:val.date, vId:val.vId, purchaseInvoiceId: e.target.value,
                      purchaseWeight:val.purchaseWeight, purchasePrice:val.purchasePrice, saleInvoiceId: val.saleInvoiceId, saleWeight:val.saleWeight, salePrice:val.salePrice} : val));
@@ -115,9 +215,6 @@ function ItemInfo(props){
         $('#'+e.target.getAttribute('id')).focusout(() => { //if enter is not pressed, and user left textfield revert changes -> (reset value to default [original])
             e.target.value=lastValue;
         })
-        
-        console.log(transaction_data);
-        
     }
 
     function changePurchaseWeight(e,lastValue,toChangeId)
@@ -144,16 +241,16 @@ function ItemInfo(props){
     }
 
 
-    let realIndex=0;
-    realIndex=ProductData.find((val,i) => pId==val.pId);    //go through JSON and list by pId and not by [index]
-    console.log('yes' +pId);
-    console.log('realIndex -> '+realIndex.pId);
+    
 
     
 
     return (
         <div className="Application">
             <header>
+                <div class="alert alert-danger" role="alert" id='error-template' onClick={()=>{$('#error-vendor').attr('hidden',true)}} hidden>
+                    This is a danger alert—check it out!
+                </div>
                 <nav className="navbar navbar-dark bg-dark">
                     <div className="container-fluid">
                     <a className="navbar-brand" href="#">Facturacion Mexico [2021 Año]</a>
@@ -330,10 +427,16 @@ function ItemInfo(props){
                                             {'->'}
                                         </th>
                                             <td>
-                                                <input type='text' id='input-row-date' className='tableInput tableDate' defaultValue={'Today'}/>
+                                                <input type='text' id='input-row-date' className='tableInput tableDate' defaultValue={moment().format('MM/DD/YYYY')}/>
                                             </td>
                                             <td>
-                                                <input type='text' id='input-row-vId' className='tableInput' onKeyDown={inputCheck}/>
+                                                <input list='vendors' type='text' id='input-row-vId' className='tableInput' onKeyDown={inputCheck}/>
+
+                                                <datalist id='vendors'>
+                                                    {VendorData.map(({vId, vName, vNumOfTransactions}) =>
+                                                        <option value={vName} id={vId}/>
+                                                    )}
+                                                </datalist>
                                             </td>
                                             <td>
                                                 <div className='tableData'>
