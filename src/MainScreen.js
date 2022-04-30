4bff38449ae8153e2d500cd4624ac3d16f9ab819
Ferb7o2 <71108160+JoseJaramillo04@users.noreply.github.com>
2022-04-29 21:10:22 -0500

    edited main screen UI

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -24,7 +24,7 @@ function MainScreen() {
 						/>
 					</div>
 					<div className="col">
-						<h1 id="underDevTitle">We're Under Development</h1>
+						<h1 id="underDevTitle">We're under Development</h1>
 						<div className="row">
 							<p id="underDevDescription">Please Come Back later!</p>
 						</div>
@@ -39,19 +39,6 @@ function MainScreen() {
 							</button>
 						</div>
 					</div>
-				</div>
-				<div className="row">
-					<p id="salesInfo">
-						<a href="https://www.google.com/search?q=sanangel+productos+mexicanos+llc&rlz=1C1ONGR_enUS962US962&oq=sana&aqs=chrome.1.69i60j69i59j69i57j69i59l2j0i271l2j69i60.2432j0j1&sourceid=chrome&ie=UTF-8">
-							SanAngel Productos Mexicanos LLC
-						</a>
-					</p>
-				</div>
-				<div className="row">
-					<p id="salesInfo">2501 W. Military Hwy</p>
-				</div>
-				<div className="row">
-					<p id="salesInfo">Ste B-15, McAllen TX 78503</p>
 				</div>
 				<div className="row">
 					<p id="salesInfo">
