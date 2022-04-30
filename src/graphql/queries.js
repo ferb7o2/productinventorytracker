221c5b284c592bbe34432fe7301aaafa1dfdd0b2
Ferb7o2 <71108160+JoseJaramillo04@users.noreply.github.com>
2022-04-29 23:25:11 -0500

    made business title data-depended

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -2,37 +2,35 @@
 // this is an auto generated file. This will be overwritten
 
 export const getOurBusinessInfo = /* GraphQL */ `
-  query GetOurBusinessInfo($id: ID!) {
-    getOurBusinessInfo(id: $id) {
-      id
-      name
-      address
-      rfc
-      createdAt
-      updatedAt
-    }
-  }
+	query GetOurBusinessInfo($id: ID!) {
+		getOurBusinessInfo(id: $id) {
+			id
+			name
+			address
+			rfc
+			createdAt
+			updatedAt
+		}
+	}
 `;
 export const listOurBusinessInfos = /* GraphQL */ `
-  query ListOurBusinessInfos(
-    $filter: ModelOurBusinessInfoFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listOurBusinessInfos(
-      filter: $filter
-      limit: $limit
-      nextToken: $nextToken
-    ) {
-      items {
-        id
-        name
-        address
-        rfc
-        createdAt
-        updatedAt
-      }
-      nextToken
-    }
-  }
+	query ListOurBusinessInfos(
+		$filter: ModelOurBusinessInfoFilterInput
+		$limit: Int
+		$nextToken: String
+	) {
+		listOurBusinessInfos(
+			filter: $filter
+			limit: $limit
+			nextToken: $nextToken
+		) {
+			items {
+				id
+				name
+				address
+				rfc
+			}
+			nextToken
+		}
+	}
 `;
