982b73a72b059526d2f070b765525b4cfe7ccf3b
JoseJaramillo04 <jose.jaramillo04@utrgv.edu>
2024-08-03 14:45:31 -0600

    Implemented tooltip telling you row is locked

--- src/components/RowLock.tsx
+++ src/components/RowLock.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { FaEdit } from "react-icons/fa";
+import React, { useState } from "react";
+import { TfiUnlock } from "react-icons/tfi";
 import "../css/RowLock.css"; // Ensure this file is in the same directory or adjust the path
 import { Tooltip } from "react-bootstrap";
 
@@ -14,15 +14,47 @@ const RowLock: React.FC<RowLockProps> = ({
 	onEditClick,
 	children,
 }) => {
+	const [tooltipStyle, setTooltipStyle] = useState<React.CSSProperties>({
+		top: 0,
+		left: 0,
+	});
+
+	const handleMouseMove = (e: React.MouseEvent) => {
+		setTooltipStyle({
+			top: e.clientY + 10,
+			left: e.clientX + 10,
+			visibility: "visible",
+			opacity: 1,
+		});
+	};
+
+	const handleMouseLeave = () => {
+		setTooltipStyle({
+			...tooltipStyle,
+			visibility: "hidden",
+			opacity: 0,
+		});
+	};
+
 	return (
 		<>
 			{isLocked ? (
 				<>
-					<td className="none-col">
-						{/*<Tooltip placement="top"> */}
+					<td
+						className="none-col"
+						onMouseMove={handleMouseMove}
+						onMouseLeave={handleMouseLeave}
+					>
 						<div className="row-lock"></div>
-						{/*</Tooltip>*/}
-						<FaEdit size={"18px"} className="editLock" onClick={onEditClick} />
+						<div className="tooltip" style={tooltipStyle}>
+							Record bloqueado, presiona el botón de desbloqueo a la derecha
+							para editar
+						</div>
+						<TfiUnlock
+							size={"18px"}
+							className="editLock"
+							onClick={onEditClick}
+						/>
 					</td>
 					{children}
 				</>
